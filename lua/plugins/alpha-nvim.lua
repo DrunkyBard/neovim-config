78909f0d0b6785cca07fe0a4059968dae3de50d9
Sattar Imamov <sattar.imamov@gmail.com>
2025-01-09 19:42:03 +0100

    - Switch windows map
    - Bee startup image

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,3 +1,116 @@
+-- https://github.com/goolord/alpha-nvim/discussions/16#discussioncomment-10127079
+
+local function darken(col, value)
+  local new_color = tonumber(string.sub(col, 2), 16) - 21 * value
+  new_color = math.max(new_color, 0)
+
+  return '#' .. string.format('%x', new_color)
+end
+
+local function getNyanCatImage()
+  local mocha = require("catppuccin.palettes").get_palette("mocha")
+  local nyanCatImage = {
+    colors = {
+      ["W"] = { fg = vim.api.nvim_get_hl_by_name("Normal", true).background },
+      ["C"] = { fg = mocha.text },
+      ["B"] = { fg = mocha.crust },
+      ["R"] = { fg = mocha.red },
+      ["O"] = { fg = mocha.peach },
+      ["Y"] = { fg = mocha.yellow },
+      ["G"] = { fg = mocha.green },
+      ["U"] = { fg = mocha.blue },
+      ["P"] = { fg = mocha.yellow },
+      ["H"] = { fg = mocha.pink },
+      ["F"] = { fg = mocha.red },
+      ["M"] = { fg = mocha.overlay0 },
+      ["V"] = { fg = mocha.lavender },
+    },
+    map = {
+      [[                                                       BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB               ]],
+      [[ RRRR                RRRRRRRRRRRRRRRR                BBPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPBB             ]],
+      [[ RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRBBPPPPPPHHHHHHHHHHHHHHHHHHHHHHHHHHPPPPPPBB           ]],
+      [[ RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRBBPPPPHHHHHHHHHHHHFFHHHHFFHHHHHHHHHHPPPPBB           ]],
+      [[ OOOORRRRRRRRRRRRRRRROOOOOOOOOOOOOOOORRRRRRRRRRRRRRBBPPHHHHFFHHHHHHHHHHHHHHHHHHHHHHHHHHHHPPBB           ]],
+      [[ OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBPPHHHHHHHHHHHHHHHHHHHHBBBBHHHHFFHHHHPPBB  BBBB     ]],
+      [[ OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBPPHHHHHHHHHHHHHHHHHHBBMMMMBBHHHHHHHHPPBBBBMMMMBB   ]],
+      [[ YYYYOOOOOOOOOOOOOOOOYYYYYYYYYYYYYYYYOOBBBBBBBBOOOOBBPPHHHHHHHHHHHHFFHHHHBBMMMMMMBBHHHHHHPPBBMMMMMMBB   ]],
+      [[ YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYBBMMMMBBBBOOBBPPHHHHHHHHHHHHHHHHHHBBMMMMMMMMBBBBBBBBMMMMMMMMBB   ]],
+      [[ YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYBBBBMMMMBBBBBBPPHHHHHHFFHHHHHHHHHHBBMMMMMMMMMMMMMMMMMMMMMMMMBB   ]],
+      [[ GGGGYYYYYYYYYYYYYYYYGGGGGGGGGGGGGGGGYYYYBBBBMMMMBBBBPPHHHHHHHHHHHHHHFFBBMMMMMMMMMMMMMMMMMMMMMMMMMMMMBB ]],
+      [[ GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGBBBBMMMMBBPPHHFFHHHHHHHHHHHHBBMMMMMMCCBBMMMMMMMMMMCCBBMMMMBB ]],
+      [[ GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGBBBBBBBBPPHHHHHHHHHHHHHHHHBBMMMMMMBBBBMMMMMMBBMMBBBBMMMMBB ]],
+      [[ UUUUGGGGGGGGGGGGGGGGUUUUUUUUUUUUUUUUGGGGGGGGGGGGBBBBPPHHHHHHHHHHFFHHHHBBMMRRRRMMMMMMMMMMMMMMMMMMRRRRBB ]],
+      [[ UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUBBPPPPHHFFHHHHHHHHHHBBMMRRRRMMBBMMMMBBMMMMBBMMRRRRBB ]],
+      [[ UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUBBPPPPPPHHHHHHHHHHHHHHBBMMMMMMBBBBBBBBBBBBBBMMMMBB   ]],
+      [[ VVVVUUUUUUUUUUUUUUUUVVVVVVVVVVVVVVVVUUUUUUUUUUUUBBBBBBPPPPPPPPPPPPPPPPPPPPBBMMMMMMMMMMMMMMMMMMMMBB     ]],
+      [[ VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVBBMMMMMMBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB       ]],
+      [[ VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVBBMMMMBBBB  BBMMMMBB          BBMMMMBB  BBMMMMBB         ]],
+      [[     VVVVVVVVVVVVVVVV                VVVVVVVVVVBBBBBBBB    BBBBBB              BBBBBB    BBBB           ]],
+    }
+  }
+
+  return nyanCatImage
+end
+
+local function getBeeImage()
+  local mocha = require("catppuccin.palettes").get_palette("mocha")
+  local color_map = {
+    [[      AAAA]],
+    [[AAAAAA  AAAA]],
+    [[AA    AAAA  AAAA        KKHHKKHHHH]],
+    [[AAAA    AAAA  AA    HHBBKKKKKKKKKKKKKK]],
+    [[  AAAAAA      AAKKBBHHKKBBYYBBKKKKHHKKKKKK]],
+    [[      AAAA  BBAAKKHHBBBBKKKKBBYYBBHHHHKKKKKK]],
+    [[        BBAABBKKYYYYHHKKYYYYKKKKBBBBBBZZZZZZ]],
+    [[    YYBBYYBBKKYYYYYYYYYYKKKKBBKKAAAAZZOOZZZZ]],
+    [[    XXXXYYYYBBYYYYYYYYBBBBBBKKKKBBBBAAAAZZZZ]],
+    [[    XXXXUUUUYYYYBBYYYYYYBBKKBBZZOOAAZZOOAAAAAA]],
+    [[  ZZZZZZXXUUXXXXYYYYYYYYBBAAAAZZOOOOAAOOZZZZAAAA]],
+    [[  ZZUUZZXXUUUUXXXXUUXXFFFFFFFFAAAAOOZZAAZZZZ  AA]],
+    [[    RRRRUUUUZZZZZZZZXXOOFFFFOOZZOOAAAAAAZZZZAA]],
+    [[    CCSSUUUUZZXXXXZZXXOOFFFFOOZZOOOOZZOOAAAA]],
+    [[    CCCCUUUUUUUUUURRRROOFFFFOOZZOOOOZZOOZZZZ]],
+    [[    CCCCUUUUUUUUSSCCCCEEQQQQOOZZOOOOZZOOZZZZ]],
+    [[    CCCCUUGGUUUUCCCCCCEEQQQQOOZZOOOOZZEEZZ]],
+    [[    RRRRGGGGUUGGCCCCCCOOOOOOOOZZOOEEZZII]],
+    [[      IIRRGGGGGGCCCCCCOOOOOOOOZZEEII]],
+    [[            GGRRCCCCCCOOOOEEEEII  II]],
+    [[                RRRRRREEEE  IIII]],
+    [[                      II]],
+    [[]],
+  }
+
+
+  local yellow = "#FAC87C"
+  local orange = "#BF854E"
+  local maroon = "#502E2B"
+  local brown = "#38291B"
+  local colors = {
+    ["A"] = { fg = mocha.rosewater },
+    ["Y"] = { fg = yellow },
+    ["B"] = { fg = darken(yellow, 5) },
+    ["X"] = { fg = darken(yellow, 20) },
+    ["U"] = { fg = darken(yellow, 25) },
+    ["F"] = { fg = darken(yellow, 35) },
+    ["O"] = { fg = darken(yellow, 45) },
+    ["K"] = { fg = maroon },
+    ["H"] = { fg = darken(maroon, 10) },
+    ["Z"] = { fg = mocha.crust },
+    ["G"] = { fg = darken(yellow, 25) },
+    ["R"] = { fg = orange },
+    ["Q"] = { fg = darken(orange, 20) },
+    ["E"] = { fg = darken(orange, 35) },
+    ["I"] = { fg = brown },
+    ["C"] = { fg = mocha.mantle },
+    ["S"] = { fg = mocha.subtext1 },
+  }
+
+  return {
+    colors = colors,
+    map = color_map
+  }
+end
+
 local function getLen(str, start_pos)
   local byte = string.byte(str, start_pos)
   if not byte then
@@ -5,6 +118,23 @@ local function getLen(str, start_pos)
   end
 
   return (byte < 0x80 and 1) or (byte < 0xE0 and 2) or (byte < 0xF0 and 3) or (byte < 0xF8 and 4) or 1
+end
+
+local function buildHeaderFromColorMap(colorMap)
+  local header = {}
+  for _, line in ipairs(colorMap) do
+    local header_line = [[]]
+    for i = 1, #line do
+      if line:sub(i, i) ~= " " then
+        header_line = header_line .. "█"
+      else
+        header_line = header_line .. " "
+      end
+    end
+    table.insert(header, header_line)
+  end
+
+  return header
 end
 
 local function colorize(header, header_color_map, colors)
@@ -36,74 +166,14 @@ local function colorize(header, header_color_map, colors)
   return colorized
 end
 
-local alpha_c = function()
+local alpha_c = function(color_map, colors)
   local alpha = require("alpha")
-
-  local mocha = require("catppuccin.palettes").get_palette("mocha")
-
   local dashboard = require("alpha.themes.dashboard")
+  local header = buildHeaderFromColorMap(color_map)
 
-  local header = {
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-    [[ ██████████████████████████████████████████████████████████████████████████████████████████████████████ ]],
-  }
-
-  local color_map = {
-    [[ WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBWWWWWWWWWWWWWW ]],
-    [[ RRRRWWWWWWWWWWWWWWWWRRRRRRRRRRRRRRRRWWWWWWWWWWWWWWWWBBPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPBBWWWWWWWWWWWW ]],
-    [[ RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRBBPPPPPPHHHHHHHHHHHHHHHHHHHHHHHHHHPPPPPPBBWWWWWWWWWW ]],
-    [[ RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRBBPPPPHHHHHHHHHHHHFFHHHHFFHHHHHHHHHHPPPPBBWWWWWWWWWW ]],
-    [[ OOOORRRRRRRRRRRRRRRROOOOOOOOOOOOOOOORRRRRRRRRRRRRRBBPPHHHHFFHHHHHHHHHHHHHHHHHHHHHHHHHHHHPPBBWWWWWWWWWW ]],
-    [[ OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBPPHHHHHHHHHHHHHHHHHHHHBBBBHHHHFFHHHHPPBBWWBBBBWWWW ]],
-    [[ OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBPPHHHHHHHHHHHHHHHHHHBBMMMMBBHHHHHHHHPPBBBBMMMMBBWW ]],
-    [[ YYYYOOOOOOOOOOOOOOOOYYYYYYYYYYYYYYYYOOBBBBBBBBOOOOBBPPHHHHHHHHHHHHFFHHHHBBMMMMMMBBHHHHHHPPBBMMMMMMBBWW ]],
-    [[ YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYBBMMMMBBBBOOBBPPHHHHHHHHHHHHHHHHHHBBMMMMMMMMBBBBBBBBMMMMMMMMBBWW ]],
-    [[ YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYBBBBMMMMBBBBBBPPHHHHHHFFHHHHHHHHHHBBMMMMMMMMMMMMMMMMMMMMMMMMBBWW ]],
-    [[ GGGGYYYYYYYYYYYYYYYYGGGGGGGGGGGGGGGGYYYYBBBBMMMMBBBBPPHHHHHHHHHHHHHHFFBBMMMMMMMMMMMMMMMMMMMMMMMMMMMMBB ]],
-    [[ GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGBBBBMMMMBBPPHHFFHHHHHHHHHHHHBBMMMMMMCCBBMMMMMMMMMMCCBBMMMMBB ]],
-    [[ GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGBBBBBBBBPPHHHHHHHHHHHHHHHHBBMMMMMMBBBBMMMMMMBBMMBBBBMMMMBB ]],
-    [[ UUUUGGGGGGGGGGGGGGGGUUUUUUUUUUUUUUUUGGGGGGGGGGGGBBBBPPHHHHHHHHHHFFHHHHBBMMRRRRMMMMMMMMMMMMMMMMMMRRRRBB ]],
-    [[ UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUBBPPPPHHFFHHHHHHHHHHBBMMRRRRMMBBMMMMBBMMMMBBMMRRRRBB ]],
-    [[ UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUBBPPPPPPHHHHHHHHHHHHHHBBMMMMMMBBBBBBBBBBBBBBMMMMBBWW ]],
-    [[ VVVVUUUUUUUUUUUUUUUUVVVVVVVVVVVVVVVVUUUUUUUUUUUUBBBBBBPPPPPPPPPPPPPPPPPPPPBBMMMMMMMMMMMMMMMMMMMMBBWWWW ]],
-    [[ VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVBBMMMMMMBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBWWWWWW ]],
-    [[ VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVBBMMMMBBBBWWBBMMMMBBWWWWWWWWWWBBMMMMBBWWBBMMMMBBWWWWWWWW ]],
-    [[ WWWWVVVVVVVVVVVVVVVVWWWWWWWWWWWWWWWWVVVVVVVVVVBBBBBBBBWWWWBBBBBBWWWWWWWWWWWWWWBBBBBBWWWWBBBBWWWWWWWWWW ]],
-  }
-
-  local colors = {
-    ["W"] = { fg = vim.api.nvim_get_hl_by_name("Normal", true).background },
-    ["C"] = { fg = mocha.text },
-    ["B"] = { fg = mocha.crust },
-    ["R"] = { fg = mocha.red },
-    ["O"] = { fg = mocha.peach },
-    ["Y"] = { fg = mocha.yellow },
-    ["G"] = { fg = mocha.green },
-    ["U"] = { fg = mocha.blue },
-    ["P"] = { fg = mocha.yellow },
-    ["H"] = { fg = mocha.pink },
-    ["F"] = { fg = mocha.red },
-    ["M"] = { fg = mocha.overlay0 },
-    ["V"] = { fg = mocha.lavender },
-  }
+  -----
+  -- header = {}
+  -----
 
   dashboard.section.header.val = header
   dashboard.section.header.opts = {
@@ -138,6 +208,8 @@ return {
 
     require('alpha').setup(dashboard.config)
 
-    alpha_c()
+    -- local image = getNyanCatImage()
+    local image = getBeeImage()
+    alpha_c(image.map, image.colors)
   end
 }
